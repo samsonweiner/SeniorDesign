011984e2d6472e3807ce9157768ce950e57bc8ef
Samson Weiner <samson.weiner@uconn.edu>
2020-02-06 22:02:13 -0500

    first random forest test, everything included

---
+++ parseDataSets.py
@@ -0,0 +1,15 @@
+#File to parse input datasets with social determinants and place them into trainData.csv
+#Data from _ year will be used to train, and data from _ year will be used to test.
+
+# Social determinants used:
+#SD1: unemployment rate
+#SD2: prescription per 100 people
+#SD3: percent of adults with a bachelors degree or higher
+
+# Label used:
+#Label: opioid overdoses per county
+
+#article_read = pd.read_csv('pandas_tutorial_read.csv', delimiter=';', names = ['my_datetime', 'event', 'country', 'user_id', 'source', 'topic'])
+#article_read[['country', 'user_id']]
+#article_read[article_read.source == 'SEO']
+#df.to_csv('data.csv')
